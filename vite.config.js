64a43d8eba79578255562b2864811c69e7a0dcf9
erGoProcsee <ernesto.gomez.338@my.csun.edu>
2024-04-29 08:41:43 -0700

    ghpages fix

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   // base: "https://erGoProcsee.github.io/portfolio_react",
-  base: "/",
+  base: "/portfolio_react/",
   plugins: [react()],
 })
